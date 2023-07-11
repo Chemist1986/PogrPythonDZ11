f34c5c9bfa94460d63fdd7a75570fde074baf651
Vyacheslav <vmk_86@mail.ru>
2023-07-11 22:35:05 +0300

    save3

---
+++ Task3.py
@@ -0,0 +1,35 @@
+class Archive:
+    number_archive = []
+    string_archive = []
+
+    def __init__(self, number, string):
+        
+        self.number = number
+        self.string = string
+        self.number_archive.append(number)
+        self.string_archive.append(string)
+
+    def get_number_archive(self):
+        return self.number_archive
+
+    def get_string_archive(self):
+        return self.string_archive
+
+    def __repr__(self):
+        return f"Archive(number={self.number}, string='{self.string}')"
+
+    def __str__(self):
+        return f"Число: {self.number}, Строка: {self.string}"
+
+
+
+archive1 = Archive(42, "Hello")
+print(repr(archive1)) 
+print(archive1) 
+
+archive2 = Archive(123, "World")
+print(repr(archive2))  
+print(archive2)  
+
+print(archive1.get_number_archive())  
+print(archive1.get_string_archive())  
